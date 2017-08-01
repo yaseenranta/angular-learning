1969fdae723546254c9dd1c7d1b25aec6298070c
mustafashoaib001@gmail.com <yaseenranta76@gmail.com>
2017-08-01 17:55:44 +0500

    Angular form practice

--- src/app/components/form.component.ts
+++ src/app/components/form.component.ts
@@ -1,8 +1,14 @@
 import {Component} from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector : 'app-yrform',
-  templateUrl : '../../../public/html/form.component.html'
+  templateUrl : '../../../public/html/form.component.html',
+  styles : [`
+        .ng-invalid.ng-touched{border-color:red;}
+        .ng-valid.ng-touched{border-color:green;}
+  `],
+  providers: [FormControl]
 })
 
 
@@ -13,10 +19,28 @@ export class YRFormComponent {
     name : '',
     email : '',
     message : '',
+    selectbox:'',
+  }
+  options = [
+    {id : 1,name: 'test 1'},
+    {id : 2,name: 'test 2'},
+    {id : 3,name: 'test 3'},
+  ];
+
+  form = {
+      username : new FormControl('', Validators.required),
+  }
+
+
+
+  constructor(FormControl: FormControl){
+
   }
 
   save(data) {
     console.log(data);
   }
+
+
 
 }
